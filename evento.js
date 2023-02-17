0a5a308c539d231784a0a2bc8e7cbcd412b743ad
LeonardoJDev <leonardojFigueroaval@gmail.com>
2023-02-17 17:27:15 +0000

    Primer comit

---
+++ evento.js
@@ -0,0 +1,23 @@
+const ordenador1={
+    idDispositivo: 1400000,
+    estadoSeguro: true,
+    fechaVencimientoSeguro: "22-03-2023",
+    nombreSeguro: "Seguros Pepe",
+    infoContactoSeguro: "segurospepe@gmail.com"
+}
+
+const ordenador2={
+    idDispositivo: 1400200,
+    estadoSeguro: true,
+    fechaVencimientoSeguro: "22-03-2024",
+    nombreSeguro: "Seguros Pepe",
+    infoContactoSeguro: "segurospepe@gmail.com"
+}
+
+const ordenador3={
+    idDispositivo: 1300000,
+    estadoSeguro: false,
+    fechaVencimientoSeguro: "22-03-2021",
+    nombreSeguro: "Seguros Pepe",
+    infoContactoSeguro: "segurospepe@gmail.com"
+}
